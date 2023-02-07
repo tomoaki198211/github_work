aa7960043801c032a4c7a652bf8de37289f9e80a
syunsuke sasaki <sasaki2931@gmail.com>
2023-02-07 17:03:10 +0900

    タスク完了

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,55 @@
+class UsersController < ApplicationController
+    skip_before_action :login_required, only: [:new, :create]
+
+
+    def index
+      @user = User.all 
+    end
+
+    def new
+      @user = User.new
+    end
+
+    def create
+        @user = User.new(user_params)
+        if @user.save
+            redirect_to user_path(@user.id)
+        else
+          render :new
+        end
+    end
+
+
+    def show
+        @user = User.find(params[:id])
+    end
+
+
+    def update
+      @user = User.find(params[:id])
+      if @user.update(user_params)
+        redirect_to user_path(@user.id), notice: "ユーザーを編集しました！"
+      else
+        render :edit
+      end
+    end
+
+    def edit
+        @user = User.find(params[:id])
+    end
+    
+    def destroy
+        @user = User.find(params[:id])
+        @user.destroy
+        redirect_to new_session_path, notice:"ユーザーを削除しました！"
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:name,:email,:password,:password_confirmation)
+    end
+
+
+
+end
