45bc51ac1f3d36c38308f5a5bc01c0b4a8c27998
kouichi shinkokura <stndard809@gmail.com>
2023-02-07 17:23:02 +0900

    blog機能2

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+
+
+
+
+
+
+
+
+
+
 ActiveRecord::Schema.define(version: 2023_02_07_081135) do
 
   # These are extensions that must be enabled in order to support this database
